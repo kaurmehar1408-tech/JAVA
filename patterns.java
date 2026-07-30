76d926eb4558da18aa3998ad5dd114bfca53e340
kaurmehar1408-tech <kaurmehar1408@gmail.com>
2026-07-30 20:16:22 +0530

    Added Inverted Pyramid

--- patterns.java
+++ patterns.java
@@ -36,14 +36,33 @@ public static void main(String[]args){
 // }
 
 ////5) Hollow Square
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=n;j++){
+//         if(i==1 || i==n || j==1 || j==n){
+//             System.out.print("*");
+//         }
+//         else{
+//             System.out.print(" ");
+//         }
+//     }
+//     System.out.println();
+// }
+
+////6) Inverted & Rotated Half Pyramid
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=n-i;j++){
+//         System.out.print(" ");
+//     }
+//     for(int j=1;j<=i;j++){
+//         System.out.print("*");
+//     }
+//     System.out.println();
+// }
+
+////7) Inverted Number Pyramid
 for(int i=1;i<=n;i++){
-    for(int j=1;j<=n;j++){
-        if(i==1 || i==n || j==1 || j==n){
-            System.out.print("*");
-        }
-        else{
-            System.out.print(" ");
-        }
+    for(int j=1;j<=n-i+1;j++){
+        System.out.print(j);
     }
     System.out.println();
 }
