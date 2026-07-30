1f6a9f0871029eef7c60aed0a880796034afca3e
kaurmehar1408-tech <kaurmehar1408@gmail.com>
2026-07-30 20:48:08 +0530

    added floyd's triangle 0-1 triangle butterfly pattern

--- patterns.java
+++ patterns.java
@@ -60,9 +60,58 @@ public static void main(String[]args){
 // }
 
 ////7) Inverted Number Pyramid
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=n-i+1;j++){
+//         System.out.print(j);
+//     }
+//     System.out.println();
+// }
+
+////8) Floyd's Triangle
+// int count = 1;
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=i;j++){
+//         System.out.print(count+" ");
+//         count++;
+//     }
+//     System.out.println();
+// }
+
+////9) 0-1 Triangle
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=i;j++){
+//         if((i+j)% 2 == 0){
+//             System.out.print(1+" ");
+//         }
+//         else{
+//             System.out.print(0+ " ");
+//         }
+//     }
+//     System.out.println();
+// }
+
+////10) Butterfly Pattern
 for(int i=1;i<=n;i++){
-    for(int j=1;j<=n-i+1;j++){
-        System.out.print(j);
+    for(int j=1;j<=i;j++){
+        System.out.print("*");
+    }
+    for(int j=1;j<=2*(n-i);j++){
+        System.out.print(" ");
+    }
+    for(int j=1;j<=i;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+for(int i=n;i>=1;i--){
+    for(int j=1;j<=i;j++){
+        System.out.print("*");
+    }
+    for(int j=1;j<=2*(n-i);j++){
+        System.out.print(" ");
+    }
+    for(int j=1;j<=i;j++){
+        System.out.print("*");
     }
     System.out.println();
 }
