cc0945542e8d4be44e5d927a9fcf383116c99b7d
kaurmehar1408-tech <kaurmehar1408@gmail.com>
2026-07-30 20:55:53 +0530

    added rhombus patterns

--- patterns.java
+++ patterns.java
@@ -91,29 +91,56 @@ public static void main(String[]args){
 // }
 
 ////10) Butterfly Pattern
-for(int i=1;i<=n;i++){
-    for(int j=1;j<=i;j++){
-        System.out.print("*");
-    }
-    for(int j=1;j<=2*(n-i);j++){
-        System.out.print(" ");
-    }
-    for(int j=1;j<=i;j++){
-        System.out.print("*");
-    }
-    System.out.println();
-}
-for(int i=n;i>=1;i--){
-    for(int j=1;j<=i;j++){
-        System.out.print("*");
-    }
-    for(int j=1;j<=2*(n-i);j++){
-        System.out.print(" ");
-    }
-    for(int j=1;j<=i;j++){
-        System.out.print("*");
-    }
-    System.out.println();
-}
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=i;j++){
+//         System.out.print("*");
+//     }
+//     for(int j=1;j<=2*(n-i);j++){
+//         System.out.print(" ");
+//     }
+//     for(int j=1;j<=i;j++){
+//         System.out.print("*");
+//     }
+//     System.out.println();
+// }
+// for(int i=n;i>=1;i--){
+//     for(int j=1;j<=i;j++){
+//         System.out.print("*");
+//     }
+//     for(int j=1;j<=2*(n-i);j++){
+//         System.out.print(" ");
+//     }
+//     for(int j=1;j<=i;j++){
+//         System.out.print("*");
+//     }
+//     System.out.println();
+// }
+
+////11) Solid rhombus
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=n-i;j++){
+//         System.out.print(" ");
+//     }
+//     for(int j=1;j<=n;j++){
+//         System.out.print("*");
+//     }
+//     System.out.println();
+// }
+
+////12) Hollow Rhombus
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=n-i;j++){
+//         System.out.print(" ");
+//     }
+//     for(int j=1;j<=n;j++){
+//         if(i==1 || i==n || j==1 || j==n){
+//             System.out.print("*");
+//         }
+//         else{
+//             System.out.print(" ");
+//         }
+//     }
+//     System.out.println();
+// }
 }
 }
