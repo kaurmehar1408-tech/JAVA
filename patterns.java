21386b01ea48186bb0001172ba94d48e1ded5629
kaurmehar1408-tech <kaurmehar1408@gmail.com>
2026-07-30 20:10:01 +0530

    added hollow square

--- patterns.java
+++ patterns.java
@@ -34,5 +34,18 @@ public static void main(String[]args){
 //     }
 //     System.out.println();
 // }
+
+////5) Hollow Square
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+        if(i==1 || i==n || j==1 || j==n){
+            System.out.print("*");
+        }
+        else{
+            System.out.print(" ");
+        }
+    }
+    System.out.println();
+}
 }
 }
